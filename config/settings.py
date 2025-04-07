dd4f5e7884a119c5368ae19dcbcca3a7f84f57cd
juraevdev <juraevdevpy@gmail.com>
2025-04-07 12:58:40 +0500

    stuff

--- config/settings.py
+++ config/settings.py
@@ -5,7 +5,7 @@ import environ, os
 
 env = environ.Env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
