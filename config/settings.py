575006a43306fb68a2b3541fffdb63ae871fb74d
juraevdev <juraevdevpy@gmail.com>
2025-04-18 11:14:49 +0500

    stuff

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(',')
 
+CSRF_COOKIE_SECURE = False
+SESSION_COOKIE_SECURE = False
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://eduflix.onrender.com",  # agar shunday bo‘lsa
+]
+
+
 
 # Application definition
 
